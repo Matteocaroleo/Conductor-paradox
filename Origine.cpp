0737449d911504a689508600d71c7db0d85e82df
Matteo <matteocaroleo57@gmail.com>
2023-03-04 22:18:58 +0100

    added source code

---
+++ Origine.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define N 1000000	//is how many times the test is done
+
+void noChangeDoor (int* results); // function that reiterates n-times without changing door and writes results on array
+void changeDoor (int* results); // function that reiterates n-times that changes door and writes results on arrray
+int menu();
+
+
+void main()
+{
+	printf("This program demonstrates the conductor paradox in an empirical way.\n");
+	int result;
+	srand(time(NULL));
+	while (true) {
+		int result = 0;
+		switch (menu())
+		{
+		case 1:
+			for (int i = 0; i < N; i++)
+			{
+				noChangeDoor(&result);
+
+			}
+			break;
+		case 2:
+			for (int i = 0; i < N; i++)
+			{
+				changeDoor(&result);
+
+			}
+			break;
+		case 3:
+			return;
+		default:
+			printf("Out of range, look at the choices in the menu\n");
+			return;
+			break;
+
+		}
+		printf("Out of %i iterations, it guessed right %i times.\n",N, result);
+	}
+	return;
+}
+
+int menu()
+{	
+	int c = 0;
+	printf("Enter 1 for playing without ever changing door : \
+			\nEnter 2 for playing changing door:\
+			\nEnter 3 to close the program ");
+	scanf_s("%i", &c);
+	return c;
+}
+
+void noChangeDoor(int* results)
+{
+
+
+	int doors[3]= {0,0,0};
+	doors[rand() % 3] = 1; //Set the car in a random door (1=car, 0=goat)
+	int choice = rand() % 3; //Set the choice
+	if (doors[choice] == 1)
+	{
+		(* results)++;
+	}
+	return;
+
+}
+
+void changeDoor(int* results)
+{
+	int doors[3] = { 0,0,0 };
+	doors[rand() % 3] = 1; //Set the car in a random door (1=car, 0=goat)
+	int firstChoice = rand() % 3; //Set the first choice
+	int reveal = rand() % 3; //The conductor chooses which door to reveal 
+	while (doors[reveal] == 1 || reveal == firstChoice)
+	{
+		reveal = rand() % 3;
+	}//the conductor can't reveal the car or the first choice
+	doors[reveal] = 2; //2 means that the door has been revealed
+	int secondChoice = rand() % 3;
+	while (doors[secondChoice] == 2) {
+		secondChoice = rand() % 3;
+	}// the second choice can't be the revealed door
+	if (doors[secondChoice] == 1) {
+		(*results)++;
+	}
+	return;
+}
+
