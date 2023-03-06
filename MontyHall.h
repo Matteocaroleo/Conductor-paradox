d08921eaffce13ea0b0893a82d2971236505cca0
Matteo <matteocaroleo57@gmail.com>
2023-03-06 19:10:48 +0100

    Added percentage of success and divided file .h from .cpp. Also small updates to documentation

---
+++ MontyHall.h
@@ -0,0 +1,28 @@
+/** @file MontyHall.h
+	@brief Header related to Origine.cpp
+
+*/
+
+/** @brief Simulates playing the game without ever changing door
+	@param results is the memory location where the result is stored
+	@return returns nothing, it directly writes in the memory location
+*/
+
+void noChangeDoor(int* results);
+
+/** @brief Simulates playing the game by always changing door
+	@param results is the memory location where the result is stored
+	@return returns nothing, it directly writes in the memory location
+*/
+void changeDoor(int* results);
+
+/** @brief Calculates the percentage of successful guesses. The total number of tests is supposed to be a costant.
+	@return Returns the percentage
+*/
+int percentageOfSuccess(int occurrences);
+
+/** @brief Simple menu function, generates the user interface
+	@return the return is used to clear the buffer if any unexpected characters get typed
+*/
+int menu();
+#pragma once
